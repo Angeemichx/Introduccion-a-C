18c258ad9b203a38712bc7b6bc1b7e6d36d790df
Katty897 <k.jimenez.3@estudiantec.cr>
2026-06-18 13:34:48 -0600

    Soluciones

--- calculadora.cs
+++ calculadora.cs
@@ -2,11 +2,28 @@ using System;
 
 class Calculadora
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.Write("Ingrese un número (float): ");
-        float angulo = float.Parse(Console.ReadLine());
-        
-        Console.WriteLine($"El ángulo ingresado es: {angulo}");
+        #aqui solo estan declarados numeros random, usted los cambia por los
+        # del usuario :)))))
+        double angulo = 0.5;
+        string operacion = "arcsin";
+
+        double resultado = 0;
+
+        if (operacion == "arcsin")
+            resultado = Math.Asin(angulo);
+        else if (operacion == "arccos")
+            resultado = Math.Acos(angulo);
+        else if (operacion == "arctan")
+            resultado = Math.Atan(angulo);
+        else if (operacion == "sinh")
+            resultado = Math.Sinh(angulo);
+        else if (operacion == "cosh")
+            resultado = Math.Cosh(angulo);
+        else if (operacion == "tanh")
+            resultado = Math.Tanh(angulo);
+
+        Console.WriteLine($"Operación: {operacion}({angulo}) = {resultado}");
     }
 }
